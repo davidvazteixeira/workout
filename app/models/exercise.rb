380c95d6bfbeaf9ff267194d866d2f9e9fc5d98d
David <dvt4qb@gmail.com>
2017-07-04 03:47:47 -0300

    validations in exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,14 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  validates :duration_in_min,
+    presence: true,
+    numericality: {only_integer: true}
+  validates :workout,
+    presence: true,
+    allow_blank: false
+  validates :workout_date,
+    presence: true,
+    allow_blank: false
+  validates :user_id,
+    presence: true
 end
--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -4,14 +4,14 @@ RSpec.feature "Creating Exercises" do
     before do
       @john = User.create!(first_name: "John",last_name: "Nash",email: "john@mail.com", password:"password")
       login_as(@john)
-    end
-
-    scenario "with valid inputs" do
       visit "/"
 
       click_link "My Lounge"
       click_link "New Workout"
       expect(page).to have_link("Back")
+    end
+
+    scenario "with valid inputs" do
 
       fill_in "Duration", with: 70
       fill_in "Workout details", with: "Weight lifting"
@@ -21,5 +21,14 @@ RSpec.feature "Creating Exercises" do
       expect(page).to have_content("Exercise has been created")
       exercise = Exercise.last
       expect(page.current_path).to eq(user_exercise_path(@john, exercise))
+    end
+
+    scenario "with invalid inputs" do
+      click_button "Create Exercise"
+      expect(page).to have_content("Exercise has not been created")
+      expect(page).to have_content("Duration in min can't be blank")
+      expect(page).to have_content("Duration in min is not a number")
+      expect(page).to have_content("Workout can't be blank")
+      expect(page).to have_content("Workout date can't be blank")
     end
 end
